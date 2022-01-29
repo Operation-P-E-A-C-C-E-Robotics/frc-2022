af82d317fce505f843479e71e77484e984da6551
Sean B <sdbazoes@gmail.com>
2022-01-28 19:32:56 -0500

    Did limeelight/aiming things

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -64,6 +64,7 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
+
   private void configureButtonBindings() {
     driveTrain.setDefaultCommand(arcadeDrive);
     intake.setDefaultCommand(runIntake);
