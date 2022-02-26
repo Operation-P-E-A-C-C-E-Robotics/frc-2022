84ca86ca00715639460663fd3f6b939c1c7cf846
Sean B <sdbazoes@gmail.com>
2022-02-26 13:21:22 -0500

    Assign buttons to intake controls

--- src/main/java/frc/robot/commands/ManualIntake.java
+++ src/main/java/frc/robot/commands/ManualIntake.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
+import frc.robot.Constants.Traversal;
 import frc.robot.subsystems.BallHandler;
 import static frc.robot.Constants.Intake.*;
 
@@ -30,19 +31,39 @@ public class ManualIntake extends CommandBase {
     int pov = container.getOperatorJoystick().getPOV();
     //System.out.println(pov);
     if (pov == 0) {
-      intake.setPercent(INTAKE_SPEED);
+      intake.setIntake(INTAKE_SPEED);
     } else if (pov == 180) {
-      intake.setPercent(-INTAKE_SPEED);
-    } else {
-      intake.setPercent(0);
+      intake.setIntake(-INTAKE_SPEED);
+    } else if (container.getDriverJoystick().getRawButton(1) == true) {
+      intake.setIntake(INTAKE_SPEED);
+    }else {
+      intake.setIntake(0);
     }
     
+
+
+    if (container.getOperatorJoystick().getRawButton(5) == true) {
+      intake.setTraversal(-0.5);
+    } else if (container.getDriverJoystick().getRawButton(2) == true) {
+      intake.setTraversal(-0.5);
+    } else {
+      intake.setTraversal(0);
+    }
+
+
+    if (container.getOperatorJoystick().getRawButton(7) == true) {
+      intake.setTrigger(-1);
+    } else {
+      intake.setTrigger(0);
+    }
+
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intake.setPercent(0);
+    intake.setIntake(0);
   }
 
   // Returns true when the command should end.
