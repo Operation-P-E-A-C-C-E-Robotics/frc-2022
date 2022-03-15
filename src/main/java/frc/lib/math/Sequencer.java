cbdef4c05540e5ec3006530260a819b3613d8811
phlogiston1 <sdbazoes@gmail.com>
2022-03-15 11:36:09 -0400

    new flywheel ready function [untested]

--- src/main/java/frc/lib/math/Sequencer.java
+++ src/main/java/frc/lib/math/Sequencer.java
@@ -1,6 +1,7 @@
 package frc.lib.math;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 public class Sequencer {
     ArrayList<Double> values = new ArrayList<>();
@@ -50,11 +51,48 @@ public class Sequencer {
         }
         return working;
     }
+    public static double[] predict(double[] computed, int numpredictions){
+        double[] current = new double[numpredictions + 1];
+        for(int i = 0; i < numpredictions; i++){
+            current[i] = computed[computed.length - 1];
+        }
+        for(int i = computed.length - 2; i >= 0; i--){
+            current = stepUp(current, computed[i]);
+        }
+        return current;
+    }
+    public static double[] compute(double[] input){
+        double[] res = new double[input.length];
+        double[] current = input;
+        for (int i = 0; i < input.length; i++){
+            res[i] = current[0];
+            current = stepDown(current);
+        }
+        return res;
+    }
+    public static double[] stepUp(double[] in, double start){
+        double[] result = new double[in.length + 1];
+        result[0] = start;
+        for(int i = 0; i < in.length; i++){
+            result[i+1] = result[i] + in[i];
+        }
+        return result;
+    }
+    public static double[] stepDown(double[] in){
+        double[] result = new double[in.length - 1];
+        for (int i = 1; i < in.length; i++){
+            result[i - 1] = in[i] - in[i - 1];
+        }
+        return result;
+    }
     public static void main(String args[]){
-        Sequencer test = new Sequencer();
-        test.setValues(new ArrayList<>(List.of(1.0,4.0,9.0,16.0)));
-        ArrayList<Double> computed = test.compute();
-        System.out.println(computed);
-        System.out.println(test.predict(computed, 1, 1));
+        // Sequencer test = new Sequencer();
+        // test.setValues(new ArrayList<>(List.of(1.0,4.0,9.0,16.0)));
+        // ArrayList<Double> computed = test.compute();
+        // System.out.println(computed);
+        // System.out.println(test.predict(computed, 1, 1));
+        double[] test = {1,1,0};
+        double[] res = Sequencer.predict(test, 50);
+        for (double i : res) System.out.println(i);
     }
 }
